3bbe0bd40ba18e31fa19eda316378c107a912b54
Kai Henseler <kai.henseler@strato.de>
2026-07-30 13:39:12 +0200

    feat: Add localization files for multiple languages
    Adds translation files in both .js and .json formats for English,
    German, Spanish, French, Italian, Dutch, and Swedish languages.
    
    Ported from legacy theming header
    
    Signed-off-by: Kai Henseler <kai.henseler@strato.de>

---
+++ l10n/it.js
@@ -0,0 +1,11 @@
+OC.L10N.register(
+    "simplenavigation",
+    {
+    "Help & Support" : "Aiuto e supporto",
+    "IONOS Webmail" : "IONOS Webmail",
+    "Login & Security" : "Accesso & sicurezza",
+    "Logout" : "Esci",
+    "Settings" : "Impostazioni",
+    "User menu" : "Menu utente"
+},
+"nplurals=2; plural=(n != 1);");
